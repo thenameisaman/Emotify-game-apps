b243d50d27cd871eb37501d4f3bd4c7107fda949
bhanu8047 <bpsbhanupratap96@gmail.com>
2021-06-03 09:38:28 +0530

    intial process-beta version1.0.0

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, Response,render_template
+from werkzeug.utils import secure_filename
+import json
+from db import db_init, db
+from model import Img
+import random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db_init(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/uploadFile')
+def uploadFile():
+    return render_template("upload.html")
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    pic = request.files['pic']
+    class_type = (request.form.to_dict()['attrib'])
+    if not pic:
+        return {
+        'success': False,
+        'message': 'File not uploaded',
+        'resCode': 400
+    }
+
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+    if not filename or not mimetype:
+        return {
+        'success': False,
+        'message': 'Bad upload.',
+        'resCode': 400
+    }
+
+    img = Img(img=pic.read(), name=filename, mimetype=mimetype, class_type = class_type)
+    db.session.add(img)
+    db.session.commit()
+
+    return {
+        'success': True,
+        'message': 'Hogyi h upload.',
+        'resCode': 200
+    }
+
+
+@app.route('/getImage/<int:id>')
+def get_img(id):
+    img = Img.query.filter_by(id=id).first()
+    if not img:
+        return 'Img Not Found!', 404
+    print(img.img)
+
+    return Response(img.img, mimetype=img.mimetype)
+
+attribute = ''
+
+@app.route('/getRandomImage')
+def getRandom():
+    queryLen = len(Img.query.all())
+    id_random = random.randint(1,queryLen)
+    img = Img.query.filter_by(id=id_random).first()
+    global attribute 
+    attribute = img.class_type
+    return Response(img.img, mimetype=img.mimetype)
+
+@app.route('/getRandomImage/attribute', methods=['GET'])
+def getAttrib():
+    return {
+        'attrib': attribute
+    }
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/JS/app.js
@@ -0,0 +1,195 @@
+const btnANGRY = document.querySelector('#btnANGRY')
+const btnSURPRISED = document.querySelector('#btnSURPRISED')
+const btnDISGUSTED = document.querySelector('#btnDISGUSTED')
+const btnNEUTRAL = document.querySelector('#btnNEUTRAL')
+const btnSAD = document.querySelector('#btnSAD')
+const btnHAPPY = document.querySelector('#btnHAPPY')
+const btnFEARFUL = document.querySelector('#btnFEARFUL')
+let counter = 0
+
+// HELPER FUNCTIONSS
+
+const addUserTimeTaken = (usedTime) => {
+    let userTime = []
+    let savedTimes = JSON.parse(localStorage.getItem("userTime"))
+    console.log(savedTimes)
+    userTime = [...savedTimes]
+    userTime.push(usedTime)
+    localStorage.setItem("userTime", JSON.stringify(userTime))
+}
+const stopTimer = () => {
+    countDown.stop()
+    countViz.stop()
+    disableInput()
+}
+
+const disableInput = () => {
+    btnANGRY.setAttribute("disabled",true)
+    btnSURPRISED.setAttribute("disabled",true)
+    btnDISGUSTED.setAttribute("disabled",true)
+    btnNEUTRAL.setAttribute("disabled",true)
+    btnSAD.setAttribute("disabled",true)
+    btnFEARFUL.setAttribute("disabled",true)
+    btnHAPPY.setAttribute("disabled",true)
+}
+const enableInput = () => {
+    btnANGRY.removeAttribute("disabled")
+    btnSURPRISED.removeAttribute("disabled")
+    btnDISGUSTED.removeAttribute("disabled")
+    btnNEUTRAL.removeAttribute("disabled")
+    btnSAD.removeAttribute("disabled")
+    btnFEARFUL.removeAttribute("disabled")
+    btnHAPPY.removeAttribute("disabled")
+}
+
+// FUNCTION FOR TIMERS 
+// THIS IS FOR TEXT TIMER
+function Countdown(elem, seconds) {
+    var that = {};
+  
+    that.elem = elem;
+    that.seconds = seconds;
+    that.totalTime = seconds * 100;
+    that.usedTime = 0;
+    that.startTime = +new Date();
+    that.timer = null;
+  
+    that.count = function() {
+      that.usedTime = Math.floor((+new Date() - that.startTime) / 10);
+  
+      var tt = that.totalTime - that.usedTime;
+      if (tt <= 0) {
+        that.elem.innerHTML = '00.00';
+        clearInterval(that.timer);
+      } else {
+        var mi = Math.floor(tt / (60 * 100));
+        var ss = Math.floor((tt - mi * 60 * 100) / 100);
+        var ms = tt - Math.floor(tt / 100) * 100;
+  
+        that.elem.innerHTML =  that.fillZero(ss) + "." + that.fillZero(ms);
+      }
+    };
+    
+    that.init = function() {
+      if(that.timer){
+        clearInterval(that.timer);
+        that.elem.innerHTML = '00.00';
+        that.totalTime = seconds * 100;
+        that.usedTime = 0;
+        that.startTime = +new Date();
+        that.timer = null;
+      }
+    };
+  
+    that.start = function() {
+      if(!that.timer){
+         that.timer = setInterval(that.count, 1);
+      }
+    };
+  
+    that.stop = function() {
+    //   console.log('usedTime = ' + countDown.usedTime);
+      addUserTimeTaken(countDown.usedTime)
+      if (that.timer) clearInterval(that.timer);
+    };
+  
+    that.fillZero = function(num) {
+      return num < 10 ? '0' + num : num;
+    };
+  
+    return that;
+  }
+//   THIS IS FOR VISUALISATION
+  function timerCountDown (){
+
+    const timer = document.getElementById("timerBar")
+    var that = {}
+    that.timer = null
+    that.width = 500
+
+    that.init = function(){
+        if(that.timer) {
+            clearInterval(that.timer)
+            timer.style.width = 500
+        }
+    }
+    that.frame = function(){
+        if(that.width <= 0){
+            clearInterval(that.timer)
+            disableInput()
+        } else {
+            --that.width
+            timer.style.width = that.width + 'px'
+        }
+    }  
+    that.start = function() {
+        if(!that.timer) {
+            that.timer = setInterval(that.frame, 10)
+        }
+    }
+    that.stop = function(){
+        if(that.timer){
+            clearInterval(that.timer)
+        }
+    }
+    return that
+}
+
+
+
+
+const updateCounter = () => {
+    const counterBtn = document.querySelector('#counter')
+    counterBtn.textContent = counter
+}
+
+const fetchAttribute = async ()=> {
+    return await fetch('/getRandomImage/attribute', {
+        method: 'GET'
+    }).then(res => res.json())
+}
+const checkInput = async (attrib)=>{
+    stopTimer()
+    const data = await fetchAttribute()
+    if(!data.attrib) {
+        let message = "something went wrong, please restart your game."
+        console.log(message);
+    } else {
+        attrib === data.attrib ? counter+=1 : ''
+    }
+    updateCounter()
+}
+
+btnANGRY.addEventListener('click', async()=>checkInput('angry'))
+btnSURPRISED.addEventListener('click',()=>checkInput('surprised'))
+btnDISGUSTED.addEventListener('click',()=>checkInput('disgusted'))
+btnNEUTRAL.addEventListener('click',()=>checkInput('neutral'))
+btnSAD.addEventListener('click',()=>checkInput('sad'))
+btnHAPPY.addEventListener('click',()=>checkInput('happy'))
+btnFEARFUL.addEventListener('click',()=>checkInput('fearful'))
+
+
+const getRandomImage = async () => {
+    const img = await fetch('/getRandomImage').then(res => res.blob())
+    return img
+}
+
+const photobox = document.querySelector('#photobox')
+const next = document.querySelector('#next')
+const time = document.querySelector('#time')
+let countDown, countViz
+next.addEventListener('click', async()=>{
+    enableInput()
+    photobox.textContent = ''
+    const img = new Image()
+    const dataUrl = await getRandomImage()
+    const myBlob = URL.createObjectURL(dataUrl)
+    img.src  = myBlob
+    img.width = '200'
+    img.height = '200'
+    photobox.appendChild(img)
+    countDown = new Countdown(time, 5)
+    countViz = new timerCountDown()
+    countDown.start()
+    countViz.start()
+})
